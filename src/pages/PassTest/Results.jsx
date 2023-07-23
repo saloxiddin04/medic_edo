6e4bcc87e930a995153ae79b01ce46ca8c6bbe98
Abdulaziz <abulacoder@gmail.com>
2023-07-23 15:06:34 +0500

    update

--- src/pages/PassTest/Results.jsx
+++ src/pages/PassTest/Results.jsx
@@ -193,7 +193,7 @@ const Results = () => {
                   scope="col"
                   className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
                 >
-                  Answered
+                  Correct
                 </th>
                 <th
                   scope="col"
