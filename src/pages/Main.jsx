a67200693a2084043c1b632398f33e728ea02e90
Abdulaziz <abulacoder@gmail.com>
2023-07-23 18:01:42 +0500

    update

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 // charts
-import { Bar, BarChart, Cell, Pie, PieChart, Tooltip } from "recharts";
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Cell,
+  Legend,
+  Pie,
+  PieChart,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 
 // icons
 import { MdOutlinePlaylistAdd } from "react-icons/md";
@@ -11,6 +22,7 @@ import { GiPlainCircle } from "react-icons/gi";
 // routes
 import { Link } from "react-router-dom";
 import { ROUTES } from "../Routes/constants";
+import { getUserData } from "../auth/jwtService";
 
 const Main = () => {
   const data = [
@@ -18,35 +30,23 @@ const Main = () => {
     { name: "Incorrect", value: 4 },
   ];
 
+  const newData = [
+    {
+      name: "Page A",
+      pv: 2400,
+    },
+    {
+      name: "Page B",
+      pv: 1398,
+    },
+    {
+      name: "Page C",
+      pv: 2000,
+    },
+  ];
+
   const COLORS = ["#1d89e4", "#ffcf00"];
   const COLORS2 = ["#1d89e4", "#ffcf00"];
-
-  const RADIAN = Math.PI / 180;
-  const renderCustomizedLabel = ({
-    cx,
-    cy,
-    midAngle,
-    innerRadius,
-    outerRadius,
-    percent,
-    index,
-  }) => {
-    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
-    const x = cx + radius * Math.cos(-midAngle * RADIAN);
-    const y = cy + radius * Math.sin(-midAngle * RADIAN);
-
-    return (
-      <text
-        x={x}
-        y={y}
-        fill="white"
-        textAnchor={x > cx ? "start" : "end"}
-        dominantBaseline="central"
-      >
-        {`${(percent * 100).toFixed(0)}%`}
-      </text>
-    );
-  };
 
   return (
     <section>
@@ -87,71 +87,157 @@ const Main = () => {
       </div>
 
       <div className="card mt-8">
-        <h1 className="text-xl mb-5">Performance & Adaptive Review</h1>
-        <div className="flex item-center gap-8">
-          <div className="flex items-center gap-10 w-1/2">
-            <PieChart width={180} height={200}>
-              <Pie
-                data={data}
-                cx="50%"
-                cy="50%"
-                labelLine={false}
-                label={renderCustomizedLabel}
-                outerRadius={80}
-                fill="#8884d8"
-                dataKey="value"
-              >
-                {data.map((entry, index) => (
-                  <Cell
-                    key={`cell-${index}`}
-                    fill={COLORS[index % COLORS.length]}
-                  />
-                ))}
-              </Pie>
-              <Tooltip />
-            </PieChart>
-            <div>
-              <h2 className="text-lg mb-5">Your accuracy:</h2>
-              <ul>
-                <li className="flex items-center gap-3 ">
-                  <GiPlainCircle className="mt-1 text-primary" size="20" />
-                  <span> Correct: 67%</span>
-                </li>
-                <li className="flex items-center gap-3 mt-2">
-                  <GiPlainCircle className="mt-1 text-yellow" size="20" />
-                  <span> Incorrect: 33% </span>
-                </li>
-              </ul>
-            </div>
-          </div>
+        {getUserData().role === "admin" ? (
+          <>
+            <h1 className="text-xl mb-5">Performance & Adaptive Review</h1>
+            <div className="flex item-center gap-8">
+              <div className="flex items-center gap-10 w-1/2">
+                <PieChart width={180} height={200}>
+                  <Pie
+                    data={data}
+                    cx="50%"
+                    cy="50%"
+                    labelLine={false}
+                    outerRadius={80}
+                    fill="#8884d8"
+                    dataKey="value"
+                  >
+                    {data.map((entry, index) => (
+                      <Cell
+                        key={`cell-${index}`}
+                        fill={COLORS[index % COLORS.length]}
+                      />
+                    ))}
+                  </Pie>
+                  <Tooltip />
+                </PieChart>
+                <div>
+                  <h2 className="text-lg mb-5">Your accuracy:</h2>
+                  <ul>
+                    <li className="flex items-center gap-3 ">
+                      <GiPlainCircle className="mt-1 text-primary" size="20" />
+                      <span> Correct: 67%</span>
+                    </li>
+                    <li className="flex items-center gap-3 mt-2">
+                      <GiPlainCircle className="mt-1 text-yellow" size="20" />
+                      <span> Incorrect: 33% </span>
+                    </li>
+                  </ul>
+                </div>
+              </div>
 
-          <div className="flex items-center gap-10 w-1/2">
-            <BarChart width={150} height={180} data={data}>
-              <Bar dataKey="value">
-                {data.map((entry, index) => (
-                  <Cell
-                    key={`cell-${index}`}
-                    fill={COLORS2[index % COLORS.length]}
+              <div className="flex items-center gap-10 w-1/2">
+                <BarChart
+                  width={300}
+                  height={300}
+                  data={newData}
+                  margin={{
+                    top: 5,
+                    right: 30,
+                    left: 20,
+                    bottom: 5,
+                  }}
+                  barSize={50}
+                >
+                  <XAxis
+                    dataKey="name"
+                    scale="point"
+                    padding={{ left: 10, right: 10 }}
                   />
-                ))}
-              </Bar>
-              <Tooltip />
-            </BarChart>
-            <div>
-              <h2 className="text-lg mb-5">Peer Comparison</h2>
-              <ul>
-                <li className="flex items-center gap-3 ">
-                  <GiPlainCircle className="mt-1 text-primary" size="20" />
-                  <span>Your accuracy: 20%</span>
-                </li>
-                <li className="flex items-center gap-3 mt-2">
-                  <GiPlainCircle className="mt-1 text-yellow" size="20" />
-                  <span>Peers accuracy: 50%</span>
-                </li>
-              </ul>
+                  <YAxis />
+                  <Tooltip />
+                  <Legend />
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <Bar
+                    dataKey="pv"
+                    fill="#8884d8"
+                    background={{ fill: "#eee" }}
+                  />
+                </BarChart>
+                <div>
+                  <h2 className="text-lg mb-5">Peer Comparison</h2>
+                  <ul>
+                    <li className="flex items-center gap-3 ">
+                      <GiPlainCircle className="mt-1 text-primary" size="20" />
+                      <span>Your accuracy: 20%</span>
+                    </li>
+                    <li className="flex items-center gap-3 mt-2">
+                      <GiPlainCircle className="mt-1 text-yellow" size="20" />
+                      <span>Peers accuracy: 50%</span>
+                    </li>
+                  </ul>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
+          </>
+        ) : (
+          <>
+            <h1 className="text-xl mb-5">Performance & Adaptive Review</h1>
+            <div className="flex item-center gap-8">
+              <div className="flex items-center gap-10 w-1/2">
+                <PieChart width={180} height={200}>
+                  <Pie
+                    data={data}
+                    cx="50%"
+                    cy="50%"
+                    labelLine={false}
+                    outerRadius={80}
+                    fill="#8884d8"
+                    dataKey="value"
+                  >
+                    {data.map((entry, index) => (
+                      <Cell
+                        key={`cell-${index}`}
+                        fill={COLORS[index % COLORS.length]}
+                      />
+                    ))}
+                  </Pie>
+                  <Tooltip />
+                </PieChart>
+                <div>
+                  <h2 className="text-lg mb-5">Your accuracy:</h2>
+                  <ul>
+                    <li className="flex items-center gap-3 ">
+                      <GiPlainCircle className="mt-1 text-primary" size="20" />
+                      <span> Correct: 67%</span>
+                    </li>
+                    <li className="flex items-center gap-3 mt-2">
+                      <GiPlainCircle className="mt-1 text-yellow" size="20" />
+                      <span> Incorrect: 33% </span>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+
+              <div className="flex items-center gap-10 w-1/2">
+                <BarChart width={150} height={180} data={data}>
+                  <Bar dataKey="value">
+                    {data.map((entry, index) => (
+                      <Cell
+                        key={`cell-${index}`}
+                        fill={COLORS2[index % COLORS.length]}
+                      />
+                    ))}
+                  </Bar>
+                  <Tooltip />
+                </BarChart>
+                <div>
+                  <h2 className="text-lg mb-5">Peer Comparison</h2>
+                  <ul>
+                    <li className="flex items-center gap-3 ">
+                      <GiPlainCircle className="mt-1 text-primary" size="20" />
+                      <span>Your accuracy: 20%</span>
+                    </li>
+                    <li className="flex items-center gap-3 mt-2">
+                      <GiPlainCircle className="mt-1 text-yellow" size="20" />
+                      <span>Peers accuracy: 50%</span>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </section>
   );
