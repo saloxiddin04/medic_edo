3df945d2ba8b16c0f95951502cb292d628141e70
saloxiddin04 <sayfuddinovsaloxiddin930@gmail.com>
2023-08-29 22:41:18 +0500

    image sizes

--- src/pages/PassTest/PastTest.jsx
+++ src/pages/PassTest/PastTest.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect, useRef } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { FcBookmark } from "react-icons/fc";
-import { FaCheck, FaCircle } from "react-icons/fa";
+import React, {useState, useEffect, useRef} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {FcBookmark} from "react-icons/fc";
+import {FaCheck, FaCircle} from "react-icons/fa";
 import Footer from "./Footer";
 import Header from "./Header";
 import {
@@ -15,35 +15,35 @@ import {
   setItem,
   getItem,
 } from "../../features/LocalStorageSlice/LocalStorageSlice";
-import { VscError } from "react-icons/vsc";
+import {VscError} from "react-icons/vsc";
 import TimeUpModal from "./TimeUpModal";
 
 const PastTest = () => {
-  const { testList, exactTest, question } = useSelector(
-    ({ pastTest }) => pastTest
+  const {testList, exactTest, question, loading} = useSelector(
+    ({pastTest}) => pastTest
   );
-  const { testID, exactTestID } = useSelector((state) => state.localStorage);
-  const { seconds } = useSelector(({ timer }) => timer);
-
+  const {testID, exactTestID} = useSelector((state) => state.localStorage);
+  const {seconds} = useSelector(({timer}) => timer);
+  
   const dispatch = useDispatch();
-
+  
   const selectedAnswerInput = document.querySelector(
     'input[name="keys"]:checked'
   );
-
+  
   const [selectedAnswer, setSelectedAnswerAnswer] = useState({
     id: null,
     key: "",
     test_question: "",
   });
-
+  
   const [countIndex, setCountIndex] = useState(0);
   const [selectedText, setSelectedText] = useState("");
   const [isTimeUp, setIsTimeUp] = useState(false);
-
+  
   const changeTest = (id, test_id, idx) => {
-    dispatch(setItem({ key: "exactTestID", value: test_id }));
-    dispatch(getExactTest({ id, test_id }));
+    dispatch(setItem({key: "exactTestID", value: test_id}));
+    dispatch(getExactTest({id, test_id}));
     dispatch(clearAnswer());
     setSelectedAnswerAnswer({
       test_question: "",
@@ -51,9 +51,9 @@ const PastTest = () => {
       id: null,
     });
     setCountIndex(idx);
-    dispatch(setItem({ key: "idx", value: idx }));
+    dispatch(setItem({key: "idx", value: idx}));
   };
-
+  
   const currentAnswer = (option) => {
     setSelectedAnswerAnswer({
       id: option.id,
@@ -61,7 +61,7 @@ const PastTest = () => {
       test_question: option.test_question,
     });
   };
-
+  
   const submitOnClick = () => {
     if (!question.is_tutor) {
       setCountIndex((prevState) =>
@@ -77,14 +77,14 @@ const PastTest = () => {
       ).then(() => {
         dispatch(getTestsById(testID));
         testList.count === countIndex + 1 &&
-          dispatch(getExactTest({ id: testID, test_id: exactTestID }));
+        dispatch(getExactTest({id: testID, test_id: exactTestID}));
         testList.count > countIndex + 1 &&
-          dispatch(
-            getExactTest({
-              id: testList?.id,
-              test_id: testList?.test_ids[countIndex + 1]?.test_question?.id,
-            })
-          );
+        dispatch(
+          getExactTest({
+            id: testList?.id,
+            test_id: testList?.test_ids[countIndex + 1]?.test_question?.id,
+          })
+        );
         setSelectedAnswerAnswer({
           test_question: "",
           key: "",
@@ -101,33 +101,33 @@ const PastTest = () => {
         })
       ).then(() => {
         dispatch(getTestsById(testID));
-        dispatch(getExactTest({ id: testID, test_id: exactTestID }));
+        dispatch(getExactTest({id: testID, test_id: exactTestID}));
       });
     }
   };
-
+  
   useEffect(() => {
-    dispatch(getItem({ key: "testID" }));
-    dispatch(getItem({ key: "exactTestID" }));
+    dispatch(getItem({key: "testID"}));
+    dispatch(getItem({key: "exactTestID"}));
   }, [dispatch]);
-
+  
   useEffect(() => {
     dispatch(getTestsById(testID));
-    dispatch(getExactTest({ id: testID, test_id: exactTestID }));
+    dispatch(getExactTest({id: testID, test_id: exactTestID}));
   }, [dispatch, exactTestID, testID]);
-
+  
   const handleSelectionChange = () => {
     const selectedText = window.getSelection().toString();
     setSelectedText(selectedText);
   };
-
+  
   useEffect(() => {
     window.addEventListener("mousemove", handleSelectionChange);
     return () => {
       window.removeEventListener("mousemove", handleSelectionChange);
     };
   }, []);
-
+  
   const timeoutId = useRef(null);
   const selectedQuestion = () => {
     if (selectedText) {
@@ -139,19 +139,19 @@ const PastTest = () => {
             text: selectedText,
           })
         ).then(() => {
-          dispatch(getExactTest({ id: testID, test_id: exactTestID }));
+          dispatch(getExactTest({id: testID, test_id: exactTestID}));
           setSelectedText("");
         });
       }, 200);
     }
   };
-
+  
   useEffect(() => {
     if (seconds <= 0) {
       setIsTimeUp(true);
     }
   }, [seconds]);
-
+  
   // if (loading) {
   //   return (
   //     <div className="w-[100wh] h-[100vh] flex justify-center items-center">
@@ -162,7 +162,7 @@ const PastTest = () => {
   //     </div>
   //   );
   // }
-
+  
   return (
     <div className="min-h-screen bg-darkLayoutStrm flex flex-wrap pb-20">
       <ul className="w-[6%] h-full overflow-y-auto bg-white border-r-2">
@@ -175,10 +175,10 @@ const PastTest = () => {
               key={index}
               className={`${index % 2 === 0 && "bg-gray-300"} 
                 h-6 flex items-center justify-center cursor-pointer ${
-                  test?.order_number === countIndex + 1
-                    ? "bg-blue-400 text-white"
-                    : ""
-                }`}
+                test?.order_number === countIndex + 1
+                  ? "bg-blue-400 text-white"
+                  : ""
+              }`}
             >
               <div className="flex relative justify-center items-center w-full">
                 <span
@@ -188,87 +188,86 @@ const PastTest = () => {
                       : "text-dark"
                   }`}
                 >
-                  {!test.is_check && <FaCircle size="6" />}
+                  {!test.is_check && <FaCircle size="6"/>}
                 </span>
                 {test.order_number}
                 <span className="absolute top-1 right-2">
                   {test.mark && (
-                    <FcBookmark className="text-white ml-1" size="14" />
+                    <FcBookmark className="text-white ml-1" size="14"/>
                   )}
                 </span>
               </div>
             </li>
           ))}
       </ul>
-
+      
       <Header
         index={countIndex}
         setIndex={setCountIndex}
         setSelectedAnswerAnswer={setSelectedAnswerAnswer}
       />
-
+      
       <div className="mt-20 p-5 overflow-y-auto w-[94%]">
         <div
           dangerouslySetInnerHTML={{
-            __html: exactTest.isFilled && question?.test_question?.question,
+            __html: question?.test_question?.question,
           }}
           onMouseUp={selectedQuestion}
         />
-        {exactTest.isFilled && question?.test_question?.image2 && (
+        {question?.test_question?.image2 && (
           <img
-            src={testList && question?.test_question?.image2}
-            className="w-full max-h-[500px] object-cover"
+            src={question?.test_question?.image2}
+            className="max-w-[50vw] max-h-[500px]"
             alt=""
           />
         )}
-
+        
         <div className="border-primary border-2 mt-10 px-5 w-full">
-          {exactTest.isFilled &&
-            question?.test_question?.options?.map((option, idx) => (
-              <label
-                className={`flex items-center gap-2 cursor-pointer my-5 ${
-                  question.is_tutor
-                    ? (option.key === question.wrong_key && `text-danger`) ||
-                      (option.key === question.right_key && `text-success`)
-                    : `${
-                        question?.is_check
-                          ? option?.key === question?.answer && `text-gray-400`
-                          : option?.key === selectedAnswer.key &&
-                            `text-gray-400`
-                      }`
-                } ${option.is_strik ? 'line-through' : ''}`}
-                htmlFor={option.key}
-                key={idx}
-                onDoubleClick={() => {
-                  dispatch(
-                    patchLineOption({
-                      start_test_id: question?.start_test_result,
-                      test_id: question?.id,
-                      option_id: option?.id,
-                      is_strik: !option.is_strik
-                    })
-                  ).then(() => {
-                    dispatch(getExactTest({ id: testID, test_id: exactTestID }))
+          {question?.test_question?.options?.map((option, idx) => (
+            <label
+              className={`flex items-center gap-2 cursor-pointer my-5 ${
+                question.is_tutor
+                  ? (option.key === question.wrong_key && `text-danger`) ||
+                  (option.key === question.right_key && `text-success`)
+                  : `${
+                    question?.is_check
+                      ? option?.key === question?.answer && `text-gray-400`
+                      : option?.key === selectedAnswer.key &&
+                      `text-gray-400`
+                  }`
+              } ${option.is_strik ? 'line-through' : ''}`}
+              htmlFor={option.key}
+              key={idx}
+              onDoubleClick={() => {
+                dispatch(
+                  patchLineOption({
+                    start_test_id: question?.start_test_result,
+                    test_id: question?.id,
+                    option_id: option?.id,
+                    is_strik: !option.is_strik
                   })
-                }}
-              >
-                <input
-                  type="radio"
-                  name="keys"
-                  id={option.key}
-                  value={option.key}
-                  disabled={question?.is_check}
-                  checked={
-                    question.answer
-                      ? option?.key === question?.answer
-                      : option.id === selectedAnswer.id
-                  }
-                  onChange={() => currentAnswer(option)}
-                />
-                <span className="uppercase">{option.key}</span>
-                <span>{option.answer}</span>
-              </label>
-            ))}
+                ).then(() => {
+                  dispatch(getExactTest({id: testID, test_id: exactTestID}))
+                })
+              }}
+            >
+              <input
+                type="radio"
+                name="keys"
+                id={option.key}
+                value={option.key}
+                disabled={question?.is_check}
+                checked={
+                  question.answer
+                    ? option?.key === question?.answer
+                    : option.id === selectedAnswer.id
+                }
+                onChange={() => currentAnswer(option)}
+              />
+              <span className="uppercase">{option.key}</span>
+              <span>{option.answer}</span>
+            </label>
+          ))}
         </div>
         <button
           className="btn-primary mt-10 inline-block"
@@ -311,19 +310,19 @@ const PastTest = () => {
                   />
                 </div>
                 {question?.test_question.image3 && (
-                  <img src={question?.test_question.image3} alt={"img"} />
+                  <img src={question?.test_question.image3} alt={"img"} className='max-w-[50vw] max-h-[500px]'/>
                 )}
                 {question?.test_question.image && (
-                  <img src={question?.test_question.image} alt={"img"} />
+                  <img src={question?.test_question.image} alt={"img"} className='max-w-[50vw] max-h-[500px]'/>
                 )}
               </div>
             )}
           </div>
         )}
       </div>
-
-      <Footer />
-      <TimeUpModal isModalOpen={isTimeUp} />
+      
+      <Footer/>
+      <TimeUpModal isModalOpen={isTimeUp}/>
     </div>
   );
 };
