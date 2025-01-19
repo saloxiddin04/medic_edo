a65400d42e25490af88379adfc9fd8374f569dec
saloxiddin04 <sayfuddinovsaloxiddin930@gmail.com>
2025-01-19 22:28:07 +0500

    load more

--- src/pages/Ranking/Ranking.jsx
+++ src/pages/Ranking/Ranking.jsx
@@ -2,6 +2,8 @@ import React, {useEffect, useState} from 'react';
 import {FaCoins, FaStar, FaUsers} from "react-icons/fa";
 import {useDispatch, useSelector} from "react-redux";
 import {getCoins, getScores} from "../../features/Ranking/RankingSlice";
+import {getUserData} from "../../auth/jwtService";
+import {AiFillEdit} from "react-icons/ai";
 
 const Ranking = () => {
 	const dispatch = useDispatch()
@@ -36,7 +38,20 @@ const Ranking = () => {
 		} else {
 			dispatch(getCoins(params))
 		}
-	}, [dispatch, activeTopTab, activeBottomTab]);
+	}, [dispatch, activeTopTab, activeBottomTab, page]);
+	
+	const getInitialsName = (name) => {
+		if (!name) return '';
+		const words = name.split(' ');
+		if (words.length === 1) {
+			return words[0].charAt(0).toUpperCase();
+		}
+		
+		return (
+			words[0].charAt(0).toUpperCase() +
+			words[1].charAt(0).toUpperCase()
+		);
+	};
 	
 	const renderRanking = () => {
 		switch (activeTopTab) {
@@ -50,11 +65,167 @@ const Ranking = () => {
 								<h1 className="mt-16">test3</h1>
 							</div>
 						</div>
+						<table className="w-full divide-y divide-gray-200 overflow-y-auto border">
+							<thead className="bg-gray-50">
+							<tr>
+								<th
+									scope="col"
+									className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+								>
+									NO
+								</th>
+								<th
+									scope="col"
+									className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+								>
+									Full Name
+								</th>
+								<th
+									scope="col"
+									className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+								>
+									Score
+								</th>
+								{getUserData().role === "admin" && (
+									<th
+										scope="col"
+										className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+									>
+										Action
+									</th>
+								)}
+							</tr>
+							</thead>
+							<tbody className="bg-white divide-y divide-gray-200">
+							{scores?.data?.map((item) => (
+								<tr key={item.id}>
+									<td className="px-6 py-4 whitespace-nowrap text-center">
+										{item?.rank}
+									</td>
+									<td className="px-6 py-4 whitespace-nowrap text-center">
+										<span
+											className="rounded-full bg-gray-300 text-dark p-2">{getInitialsName(item?.name)}</span> {item.name}
+									</td>
+									<td
+										className="px-6 py-4 whitespace-nowrap text-center text-sm text-gray-500 flex items-center gap-1 justify-center">
+										{item.score} <FaStar size={20} color="rgb(255 207 0)"/>
+									</td>
+									{getUserData().role === 'admin' && (
+										<td
+											className="px-6 py-4 whitespace-nowrap text-center text-sm font-medium"
+										>
+											<button
+												className="btn-warning btn-sm ml-3"
+											>
+												<AiFillEdit/>
+											</button>
+										</td>
+									)}
+								</tr>
+							))}
+							</tbody>
+						</table>
+						
+						<div className="mt-5 flex justify-center">
+							<button
+								className="bg-primary p-2 rounded text-white disabled:opacity-25"
+								onClick={() => {
+									if (scores?.per_page !== page) {
+										setPage(prevState => prevState + 1)
+									}
+								}}
+								disabled={scores?.per_page === page}
+							>
+								Load more
+							</button>
+						</div>
 					</>
 				)
 			case 2:
 				return (
-					<></>
+					<>
+						<div className="flex justify-center flex-wrap mt-4">
+							<div className="w-full md:w-2/6 flex justify-evenly items-start ranking h-[250px]">
+								<h1 className="mt-5">test2</h1>
+								<h1 className="-mt-5">test1</h1>
+								<h1 className="mt-16">test3</h1>
+							</div>
+						</div>
+						<table className="w-full divide-y divide-gray-200 overflow-y-auto border">
+							<thead className="bg-gray-50">
+							<tr>
+								<th
+									scope="col"
+									className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+								>
+									NO
+								</th>
+								<th
+									scope="col"
+									className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+								>
+									Full Name
+								</th>
+								<th
+									scope="col"
+									className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+								>
+									Coins
+								</th>
+								{getUserData().role === "admin" && (
+									<th
+										scope="col"
+										className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+									>
+										Action
+									</th>
+								)}
+							</tr>
+							</thead>
+							<tbody className="bg-white divide-y divide-gray-200">
+							{coins?.data?.map((item) => (
+								<tr key={item.id}>
+									<td className="px-6 py-4 whitespace-nowrap text-center">
+										{item?.rank}
+									</td>
+									<td className="px-6 py-4 whitespace-nowrap text-center">
+										<span
+											className="rounded-full bg-gray-300 text-dark p-2">{getInitialsName(item?.name)}</span> {item?.name}
+									</td>
+									<td
+										className="px-6 py-4 whitespace-nowrap text-center text-sm text-gray-500 flex items-center gap-1 justify-center">
+										{item?.coin} <FaCoins size={20} color="rgb(255 207 0)"/>
+									</td>
+									{getUserData().role === 'admin' && (
+										<td
+											className="px-6 py-4 whitespace-nowrap text-center text-sm font-medium"
+										>
+											<button
+												className="btn-warning btn-sm ml-3"
+											>
+												<AiFillEdit/>
+											</button>
+										</td>
+									)}
+								</tr>
+							))}
+							</tbody>
+						</table>
+						
+						<div className="mt-5 flex justify-center">
+							<button
+								className="bg-primary p-2 rounded text-white disabled:opacity-25"
+								onClick={() => {
+									if (coins?.per_page !== page) {
+										setPage(prevState => prevState + 1)
+									}
+								}}
+								disabled={coins?.per_page === page}
+							>
+								Load more
+							</button>
+						</div>
+					</>
 				)
 			default:
 				return <h1>Wrong tab</h1>
@@ -108,13 +279,6 @@ const Ranking = () => {
 			</div>
 			
 			{renderRanking()}
-			<div className="mt-5 flex justify-center">
-				<button
-					className="bg-primary p-2 rounded text-white"
-				>
-					Load more
-				</button>
-			</div>
 		</div>
 	);
 };
