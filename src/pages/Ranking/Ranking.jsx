2fe61e065cc66d644ba3265b814e4a60b785b3b1
saloxiddin04 <sayfuddinovsaloxiddin930@gmail.com>
2025-01-20 00:09:36 +0500

    ranking

--- src/pages/Ranking/Ranking.jsx
+++ src/pages/Ranking/Ranking.jsx
@@ -82,13 +82,59 @@ const Ranking = () => {
 			case 1:
 				return (
 					<>
-						<div className="flex justify-center flex-wrap mt-4">
-							<div className="w-full md:w-2/6 flex justify-evenly items-start ranking h-[250px]">
-								<h1 className="mt-5">test2</h1>
-								<h1 className="-mt-5">test1</h1>
-								<h1 className="mt-16">test3</h1>
+						<div className="flex flex-col items-center my-6">
+							<h2 className="text-2xl font-bold mb-1">Ranking</h2>
+							<div className="flex justify-evenly items-end relative w-full h-[300px] ranking">
+								{/* 2nd Place */}
+								<div className="flex flex-col items-center">
+									<div>
+										<FaTrophy size={50} color="rgb(209 213 219)"/>
+									</div>
+									<div
+										className="bg-gray-300 w-16 h-16 rounded-full flex items-center justify-center text-xl font-bold">
+										{getInitialsName(scores?.data[1]?.name)}
+									</div>
+									<div
+										className="bg-gray-200 text-center w-[95%] h-32 flex items-center justify-center rounded-t-md shadow-md"
+									>
+										<h3 className="text-lg font-semibold">{scores?.data[1]?.name}</h3>
+									</div>
+								</div>
+								
+								{/* 1st Place */}
+								<div className="flex flex-col items-center w-[20%]">
+									<div>
+										<FaTrophy size={50} color="rgb(255 207 0)"/>
+									</div>
+									<div
+										className="bg-yellow w-16 h-16 rounded-full flex items-center justify-center text-xl font-bold">
+										{getInitialsName(scores?.data[0]?.name)}
+									</div>
+									<div
+										className="bg-yellow text-center w-[95%] h-48 flex items-center justify-center rounded-t-md shadow-md"
+									>
+										<h3 className="text-lg font-semibold">{scores?.data[0]?.name}</h3>
+									</div>
+								</div>
+								
+								{/* 3rd Place */}
+								<div className="flex flex-col items-center">
+									<div>
+										<FaTrophy size={50} color="rgb(253 186 116)"/>
+									</div>
+									<div
+										className="bg-orange-300 w-16 h-16 rounded-full flex items-center justify-center text-xl font-bold">
+										{getInitialsName(scores?.data[2]?.name)}
+									</div>
+									<div
+										className="bg-orange-300 text-center w-[95%] h-24 flex items-center rounded-t-md shadow-md"
+									>
+										<h3 className="text-lg font-semibold">{scores?.data[2]?.name}</h3>
+									</div>
+								</div>
 							</div>
 						</div>
+						
 						<table className="w-full divide-y divide-gray-200 overflow-y-auto border">
 							<thead className="bg-gray-50">
 							<tr>
@@ -257,7 +303,6 @@ const Ranking = () => {
 								</div>
 							</div>
 						</div>
-						
 						
 						<table className="w-full divide-y divide-gray-200 overflow-y-auto border">
 							<thead className="bg-gray-50">
