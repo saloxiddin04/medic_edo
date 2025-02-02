f4cff94bd05e2995095d9a127aa98025297e302e
saloxiddin04 <sayfuddinovsaloxiddin930@gmail.com>
2025-02-02 12:53:30 +0500

    bugs fixed

--- src/pages/Auth/SignIn.jsx
+++ src/pages/Auth/SignIn.jsx
@@ -21,6 +21,7 @@ const SignIn = () => {
         setTimeout(() => {
           toast.success("You have successfully logged in");
         }, 200);
+        toast.success('click')
       })
       .catch((err) => {
         toast.error(err.response.data.detail || err.message);
